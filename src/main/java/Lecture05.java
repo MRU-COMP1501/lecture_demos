e37ec67f4f59b37f480f4ba14f41c9d9288bf211
Charlotte Curtis <c.f.curtis@gmail.com>
2021-09-23 22:03:14 -0600

    More lecture 5 method stubs, probably too many at this point

--- src/main/java/Lecture05.java
+++ src/main/java/Lecture05.java
@@ -1,10 +1,14 @@
 public class Lecture05 {
-    public void stringConcat() {
-        // Build the string "Java"
+    public void unicodeExample() {
+        // Print out a symbol from https://en.wikipedia.org/wiki/List_of_Unicode_characters
     }
 
     public void stringNumConcat() {
-        // Build the date string
+        // Build the date string and show length
+    }
+
+    public void subString() {
+        // You can't spell awesome without me
     }
 
     public void tabsAndNewlines() {
@@ -12,14 +16,26 @@ public class Lecture05 {
     }
 
     public void tabsWithLongWords() {
-        //Supercalifragilisticexpialidocious with a shorter word
+        // Supercalifragilisticexpialidocious with a shorter word
     }
 
-    public void leftJustify() {
-        // Left justify and pad to 40 spaces
+    public void printVsPrintln() {
+        // Show what happens when you use print instead of println, then add newline
+    }
+
+    public void multipleFormattedOutputs() {
+        // Print a mix of integers and doubles in printf
     }
 
     public void scientificNotation() {
         // print out the speed of light
+    }
+
+    public void leftJustify() {
+        // Left justify Supercalifragilisticexpialidocious and pad to 40 spaces
+    }
+
+    public void inputDemo() {
+        // create a scanner and read some stuff
     }
 }
--- src/main/java/LectureDemos.java
+++ src/main/java/LectureDemos.java
@@ -1,6 +1,5 @@
 public class LectureDemos {
     static public void main(String[] args) {
         Lecture05 lecture = new Lecture05();
-        lecture.stringConcat();
     }
 }
