89ad91f6c0a54e097440f7373069febda59b4c5d
Charlotte Curtis <c.f.curtis@gmail.com>
2021-09-27 08:35:55 -0600

    Adding method stubs for lecture 06

---
+++ src/main/java/Lecture06.java
@@ -0,0 +1,29 @@
+public class Lecture06 {
+    public void ioStringsReview() {
+        // go over stuff about i/o and strings
+    }
+
+    public void elevatorExample() {
+        // User inputs floor, calculate actual floor by skipping 13
+    }
+
+    public void equalsVsAssignment() {
+        // Demo equals vs assignment
+    }
+
+    public void braceDemo() {
+        // Show what happens with different brace configurations
+    }
+
+    public void semicolonDemo() {
+        // Show what happens with semicolons
+    }
+
+    public void variableScope() {
+        // Demonstrate variables in and out of scope
+    }
+
+    public void floatingComparison() {
+        // Demonstrate floating point comparison sum of 5.6 and 5.8
+    }
+}
--- src/main/java/LectureDemos.java
+++ src/main/java/LectureDemos.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class LectureDemos {
     static public void main(String[] args) {
-        Lecture05 lecture = new Lecture05();
+        Lecture04 l04 = new Lecture04();
+        Lecture05 l05 = new Lecture05();
     }
 }
