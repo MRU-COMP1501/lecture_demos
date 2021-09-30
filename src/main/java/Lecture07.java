4bd0b2411d779d49a95649ffdde920ce38a677c3
Charlotte Curtis <c.f.curtis@gmail.com>
2021-09-30 17:14:27 -0600

    Working on lecture 07

--- src/main/java/Lecture07.java
+++ src/main/java/Lecture07.java
@@ -1,3 +1,26 @@
-public class Lecture07 {
+import java.util.Scanner;
 
+public class Lecture07 {
+    public void caffeineDemo() {
+        // 0-50, 50-100,100-200,200+
+    }
+
+    public void caffeineWithoutElse() {
+    }
+
+    public void caffeineSwitch() {
+        // caffeine example with switch syntax
+    }
+
+    public void switchOddities() {
+        // demo without break or statement
+    }
+
+    public void enumDemo() {
+        // enum with drink order
+    }
+
+    public void nestedIf() {
+        // Drink order
+    }
 }
--- src/main/java/LectureDemos.java
+++ src/main/java/LectureDemos.java
@@ -3,8 +3,9 @@ public class LectureDemos {
         Lecture04 l04 = new Lecture04();
         Lecture05 l05 = new Lecture05();
         Lecture06 l06 = new Lecture06();
+        Lecture07 l07 = new Lecture07();
 
         // Change this line to call the method you want to test
-        l06.elevatorExample();
+        l07.enumDemo();
     }
 }
