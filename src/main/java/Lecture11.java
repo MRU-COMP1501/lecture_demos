9300ebfa141ab0ae16754e49baada7c43b12064b
Charlotte Curtis <c.f.curtis@gmail.com>
2021-10-27 09:22:38 -0600

    Switching palindrome detector to simpler loop

--- src/main/java/Lecture11.java
+++ src/main/java/Lecture11.java
@@ -92,10 +92,30 @@ public class Lecture11 {
 
     void palindromeDetector() {
         // Prompts the user to enter a word and outputs if it is a palindrome or not
-        System.out.println("Enter a word");
+        System.out.print("Enter a word: ");
+        String word = input.next();
+        String reversed = "";
+        int length = word.length();
+
+        for (int i = 0; i < length; i++) {
+            reversed = word.substring(i,i+1) + reversed;
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome!");
+        } else {
+            System.out.println(word + " is not a palindrome :(");
+        }
+    }
+
+    void palindromeHalfLoopDetector() {
+        // Prompts the user to enter a word and outputs if it is a palindrome or not
+        // Loops through half the word only
+        System.out.print("Enter a word: ");
         String word = input.next();
         int length = word.length();
         boolean palindrome = true;
+
         for (int i = 0; i < length / 2; i++) {
             char first = word.charAt(i);
             char last = word.charAt(length - 1 - i);
